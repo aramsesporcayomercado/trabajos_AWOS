09f6f0d7959c93bf799735985bbb024e36aeef2f
aramsesporcayomercado <94558953+aramsesporcayomercado@users.noreply.github.com>
2022-09-20 22:44:32 -0500

    Add files via upload

---
+++ operacion/src/main/java/utils/MYSQLConnection.java
@@ -0,0 +1,31 @@
+package utils;
+
+import org.apache.xmlrpc.webserver.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MYSQLConnection {
+
+    public Connection getConnection() {
+        final String DBNAME = "animals",
+                USERNAME = "root",
+                PASSWORD = "root",
+                TIMEZONE = "America/Mexico_City",
+                USESSL = "false",
+                PUBLICKEY = "true";
+        String dataSource = String.format("jdbc:mysql://localhost:3306/%s?user=%s" +
+                        "&password=%s&serverTimezone=%s&useSSL=%s&allowPublicKeyRetrieval=%s",
+                DBNAME, USERNAME, PASSWORD, TIMEZONE, USESSL, PUBLICKEY);
+        try {
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            return (Connection) DriverManager.getConnection(dataSource);
+        } catch (SQLException e) {
+            System.out.println(this.getClass().getCanonicalName() +
+                    "->" + e.getMessage());
+        }
+        return null;
+    }
+
+
+}
